9dd7972b5a27482c1cd12fd0e63fc2e905857131
koteyka <tihonenkovk@mail.ru>
2019-09-24 21:47:28 +0700

    Дворфы чёрного железа похозяйничали здесь и сделали так, чтобы твой проект не собрался. Исправь свой проект так, чтобы он не только начал собираться, но и всё в методе main работало.

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,7 @@
 public class Cat extends Animals {
+    //http://developer.alexanderklimov.ru/android/java/extends.php
     @Override
     void jump() {
- System.out.println("Прыгать");
+        System.out.println("Прыгать");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 public class Main {
 
-public String name [] = {"Pers", "Masha"};
+public String name [] = {"Pers", "Masha", "Fishka", "Barsik"};
 
-
+//Добавь файл gitignore
+//https://github.com/github/gitignore/blob/master/Java.gitignore
     public static void main(String[] args) {
 
-        Cat ourcat = new Cat();
+        Cat ourcat = new Cat(name[3]);
         ourcat.jump();
+        ourcat.scratch();
 
+        play_sound cat1 = new Cat(name[1]);
+
+        cat1.print();
+
+        Animals cat2 = new Cat(name[0]);
+
+        cat2.getName();
+
+        ability_to_scratch cat3 = new Cat(name[4]);
+
+        cat3.scratch();
 	// write your code here
     }
 }
